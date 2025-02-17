ab488f4d088a8d47a3164599aa256794ed3557ff
Chris Schultz <Chris@pyxldesign.com>
2025-02-17 15:52:42 -0500

    Fixed portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
 import { Services } from "@/components/Services";
-import { Portfolio } from "@/components/Porfolio";
+import { Portfolio } from "@/components/Portfolio";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 

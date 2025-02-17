a9f8d3e5cb1a4ded390922ac585699b3efb17df3
Chris Schultz <Chris@pyxldesign.com>
2025-02-17 15:54:53 -0500

    Changed iframe width from %

--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -18,9 +18,10 @@ export const Portfolio = () => {
                     <div className="bg-zinc-900 rounded-lg p-6 purple-glow-box">
                         <h3 className="text-2xl font-bold mb-4 text-purple-400">Featured Work</h3>
                         <iframe
-                            width="100%"
+                            width="600"
                             height="600"
                             allow="autoplay"
+                            className="w-full"
                             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1734879357&color=%236b46c1&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
                         ></iframe>
                     </div>
